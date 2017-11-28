cec2a0b2512d5fc89ce2bd55b6b0fccd3a3975fe
Ryan Orsinger <ryan.orsinger@gmail.com>
2017-11-28 09:14:15 -0600

    Use method overloading to add an optional prompt input to each Input method.

--- src/util/Input.java
+++ src/util/Input.java
@@ -13,7 +13,23 @@ public class Input {
         return this.scan.nextLine();
     }
 
+    public String getString(String prompt) {
+        System.out.println(prompt);
+        return this.scan.nextLine();
+    }
+
+
     public boolean yesNo() {
+        String input = this.scan.next();
+        if(input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yes")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean yesNo(String prompt) {
+        System.out.println(prompt);
         String input = this.scan.next();
         if(input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yes")) {
             return true;
@@ -34,7 +50,24 @@ public class Input {
         }
     }
 
+    public int getInt(int min, int max, String prompt) {
+        System.out.println(prompt);
+
+        int userInput = this.getInt();
+
+        if(userInput > max || userInput < min) {
+            return getInt(min, max);
+        } else {
+            return userInput;
+        }
+    }
+
     public int getInt() {
+        return this.scan.nextInt();
+    }
+
+    public int getInt(String prompt) {
+        System.out.println(prompt);
         return this.scan.nextInt();
     }
 
@@ -50,7 +83,24 @@ public class Input {
         }
     }
 
+    public double getDouble(double min, double max, String prompt) {
+        System.out.println(prompt);
+
+        double userInput = this.getDouble();
+
+        if(userInput > max || userInput < min) {
+            return getDouble(min, max);
+        } else {
+            return userInput;
+        }
+    }
+
     public double getDouble() {
+        return this.scan.nextDouble();
+    }
+
+    public double getDouble(String prompt) {
+        System.out.println(prompt);
         return this.scan.nextDouble();
     }
 }
