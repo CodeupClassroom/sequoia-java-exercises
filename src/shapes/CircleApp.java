8e26d4d1c3163ccc95a92ab74ea0960372237ebe
Ryan Orsinger <ryan.orsinger@gmail.com>
2017-11-28 09:19:32 -0600

    Add bonus solutions to Circle class and app

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -8,14 +8,20 @@ import util.Input;
 public class CircleApp {
     public static void main(String[] args) {
         System.out.println("Let's create a circle!");
+        Input input = new Input();
 
-        System.out.println("Please provide a radius for your circle");
-        double radius = Input.getInt();
+        do {
+            double radius = input.getInt("Please provide a radius for your circle");
 
-        Circle myCircle = new Circle(radius);
+            Circle myCircle = new Circle(radius);
 
-        System.out.println("Here's the area: " + myCircle.getArea());
+            System.out.println("Here's the area: " + myCircle.getArea());
 
-        System.out.println("Here's the circumference: " + myCircle.getCircumference());
+            System.out.println("Here's the circumference: " + myCircle.getCircumference());
+
+        } while(input.yesNo("Would you like to make another circle? y/n"));
+
+        int circleCount = Circle.getCircleCount();
+        System.out.println("Thanks for using CircleApp™. You created " + circleCount + " circle objects.");
     }
 }
